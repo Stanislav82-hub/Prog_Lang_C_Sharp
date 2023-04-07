cd4a468137c03a4503161d8d0451bd2b8341bf14
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-07 12:29:20 +0300

    Project_7_Cattlebell

---
+++ Project_7_Cattlebell/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 3;
+int c = 6;
+int d = 12;
+int e = 7;
+
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max=");
+Console.Write(max);
