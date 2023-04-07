b48a6cffc956dec0f1dc204ff80e3318988bee9a
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-07 11:54:31 +0300

    Project_6_Condition_If_Else

---
+++ Project_6_Condition_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "наташа")
+{
+   Console.WriteLine("Привет, Натусик-веселусик!!!)))");
+}
+else
+{
+    Console.WriteLine("Здарова, ");
+    Console.WriteLine(username);
+}
