adb87d350fc0f48e363b47f0320e5fc74e610a06
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-07 08:46:12 +0300

    Project_StasSum

---
+++ Project_StasSum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Ввeдите число А ");
+string? numberA = Console.ReadLine();
+Console.Write("Ввeдите число Б ");
+string? numberB = Console.ReadLine();
+Console.Write(numberA + numberB);
