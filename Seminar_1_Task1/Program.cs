bad91cfcf4d684fc812b29997cbf7b55f10050f0
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-09 10:02:41 +0300

    Seminar_1_Task1

---
+++ Seminar_1_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please fill in a number");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please fill in second number");
+int number2 = int.Parse(Console.ReadLine());
+int result = number1 / number2;
+Console.WriteLine(result);
+if(result == number2)
+{
+    Console.Write(number1);
+    Console.Write(" is a square of ");
+    Console.Write(number2);
+}
+else
+{
+    Console.Write(number1);
+    Console.Write(" is not a square of ");
+    Console.Write(number2);
+}
