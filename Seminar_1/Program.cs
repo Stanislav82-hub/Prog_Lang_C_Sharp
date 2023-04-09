ed4efb17c64ead0a2b6d045ba7814f4a73e2ed87
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-09 06:35:33 +0300

    Seminar_1:Have changed (целое число) - int for (дробное) -double

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Fill in number to findout its square");
-int number = int.Parse(Console.ReadLine());
-int result = number * number;
+﻿Console.WriteLine("Fill in number to findout its square:");
+double number = double.Parse(Console.ReadLine());
+double result = number * number;
 Console.Write("Square of ");
 Console.Write(number);
 Console.Write(" is ");
