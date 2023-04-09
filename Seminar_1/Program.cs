e4b892891bbe3e8f0cbc4e7bc5b76720e93ed21d
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-09 06:09:53 +0300

    Seminar_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Fill in number to findout its square");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.Write("Square of ");
+Console.Write(number);
+Console.Write(" is ");
+Console.WriteLine(result);
+
