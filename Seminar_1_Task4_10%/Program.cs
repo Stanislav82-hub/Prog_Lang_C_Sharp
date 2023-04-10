df445c8cdb40b8a812fed8dac687d10ee4846795
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-10 15:56:34 +0300

    Seminar_1_Task4_10%: Teaher's version

--- Seminar_1_Task4_10%/Program.cs
+++ Seminar_1_Task4_10%/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.WriteLine("Please type in a number: ");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает последнюю цифру этого числа.
+
+//Console.WriteLine("Please type in a number: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("An answer is: ");
+//Console.WriteLine(a%10); 
+// На верху то, что сами на семинаре придумали, а с низу как должно быть):
+
+Console.WriteLine("Please type in a number: ");
 int a = int.Parse(Console.ReadLine());
-Console.Write("An answer is: ");
-Console.WriteLine(a%10); 
+if (a>=100 && a < 1000){   // && - означает "и" в условии, т.е. а больше или равно 100 "И" а меньше 1000
+    Console.WriteLine(a % 10);
+}
+else{
+    Console.Write("Wrong number");
+}
