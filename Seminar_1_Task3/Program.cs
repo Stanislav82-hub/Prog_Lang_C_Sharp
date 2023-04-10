4792b08bacfdbdc09ba9c7ea1e7df2048ed80fcf
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-10 15:01:03 +0300

    Seminar_1_Task3

---
+++ Seminar_1_Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//for (int i = 0; i < 10; i++) сосотоит из инициализации счетчика; условия; прирощение(увиличение) счетчика
+
+//int i = 11;
+//System.Console.WriteLine(i % 3);
+Console.WriteLine("Введите число:");   
+int a = int.Parse(Console.ReadLine());
+for (int b = -a; b < a ; b++)
+{
+    Console.Write(b+" ");
+} 
+
+
+
+
+
