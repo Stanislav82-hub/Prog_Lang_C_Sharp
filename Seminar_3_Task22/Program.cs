978a705e0eb229dcaa5e6bcca6382fc54793ada6
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-16 15:37:09 +0300

    Seminar_3_Task22

---
+++ Seminar_3_Task22/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+Console.Write("Hi, give me a number \t");
+int a = int.Parse(Console.ReadLine());
+for (int i=1; i <= a; i++);
+{
+    Console.WriteLine("Line of numbers is "+Math.Pow(i, 2));
+}
