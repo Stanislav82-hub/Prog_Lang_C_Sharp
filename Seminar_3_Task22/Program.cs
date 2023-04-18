d1ce82ff469f240377c3e06363a8c2cb3c5c9d2a
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-18 11:21:32 +0300

    Commit for fixing

--- Seminar_3_Task22/Program.cs
+++ Seminar_3_Task22/Program.cs
@@ -2,7 +2,7 @@
 // квадратов чисел от 1 до N.
 Console.Write("Hi, give me a number \t");
 int a = int.Parse(Console.ReadLine());
-for (int i=1; i <= a; i++);
-{
-    Console.WriteLine("Line of numbers is "+Math.Pow(i, 2));
-}
+for (int i = 1; i <= a; i++)
+  {
+    System.Console.Write(" "+Math.Pow(i,2));
+  }
