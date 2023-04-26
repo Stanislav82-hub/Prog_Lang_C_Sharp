f447d1bbf5239a41265e33bd7124829bd7c45250
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-26 13:09:32 +0300

    Seminar_4_Task_28

---
+++ Seminar_4_Task_28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.Write("Hello, give me a number \t");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(MultOfDigits(number));
+
+
+int MultOfDigits(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
