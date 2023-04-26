9c13d5cfbf13edbac8b834081a3f95a9a57c97ed
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-26 13:12:45 +0300

    Seminar_4_Task_26_Math

---
+++ Seminar_4_Task_26_Math/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задача 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Write("Hello, give me a number \t");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"This number contains {DigitQty(number)} digits");
+
+int DigitQty(int number)
+{
+    int count = 1;
+    while (number > 9)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+
+
+
