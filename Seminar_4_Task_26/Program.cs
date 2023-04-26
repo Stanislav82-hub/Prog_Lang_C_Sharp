5f0795a35f18a9c94d3603c669761120372ade9e
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-26 13:10:38 +0300

    Seminar_4_Task_26

---
+++ Seminar_4_Task_26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+
+int number = FillInNumber();
+
+Console.WriteLine(DigitsQty(number));
+
+int DigitsQty(int number)
+{
+    string text = number.ToString();
+    int result = text.Length;
+    return result;
+}
+
+int FillInNumber()
+{
+     Console.Write("Hello, give me a number \t");
+     return int.Parse(Console.ReadLine());
+}
