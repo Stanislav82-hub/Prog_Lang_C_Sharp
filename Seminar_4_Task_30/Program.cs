9088f3be0f8840ff2c5936ca8cd140247f90fc1e
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-27 09:12:29 +0300

    Seminar_4_Task_30

---
+++ Seminar_4_Task_30/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] RandomArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+        array[i] = (new Random().Next(0, 2));
+    return array;
+}
+Console.Write("Fill in number \t");
+int number = int.Parse(Console.ReadLine());
+Console.Write(String.Join(", ", RandomArray(number)));
