cfbba93391310e37df878becf29e5f0606f973f0
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-28 16:29:23 +0300

    Commit corrections

---
+++ Seminar_5_Task_35/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задайте одномерный массив, состоящий из случайных чисел. Найдите количество элементов массива,
+// значения которых лежат в отрезке [10,99].
+
+void NewRamdomArray()
+{
+    int index = 12;
+    int[] array = new int[index];
+    FillRandomNumber(array);
+    PrintArray(array);
+    FillRandomNumber(array);
+    Console.WriteLine($"Numbers quantity is {CountElement(array)}");
+}
+
+void FillRandomNumber(int[] array)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-20, 150);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+
+    }
+    Console.WriteLine("");
+}
+
+int CountElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+NewRamdomArray();
