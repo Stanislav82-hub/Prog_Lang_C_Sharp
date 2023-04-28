10869172b7d2f9d3c5987dffa31ef583c002b002
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-04-28 12:06:14 +0300

    Seminar_5_Task_32

---
+++ Seminar_5_Task_32/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие
+//отрицательные и наоборот.
+void NewRamdomArray()
+{
+    int index = 12;
+    int[] array = new int[index];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-9, 10);
+    }
+    PrintArray(array);
+    ConvertToOposite(array);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+
+    }
+    Console.WriteLine("");
+}
+
+void ConvertToOposite(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+     
+    }
+    PrintArray(array);
+}
+
+NewRamdomArray();
