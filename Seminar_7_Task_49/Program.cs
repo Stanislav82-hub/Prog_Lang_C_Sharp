d3a364b59911d439fd48f395fcea8cc69cfb3e40
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-05-20 15:45:35 +0300

    Seminar_7_Task_49

---
+++ Seminar_7_Task_49/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы
+// на их квадраты.
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+int[,] NewArray = CreateTwoDimensionArray(10, 10);
+
+void ChangeEven(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (j % 2 == 0)
+                {
+                    array[i, j] = array[i, j] * array[i, j];
+                }
+
+            }
+
+
+
+        }
+    }
+
+    Console.WriteLine();
+
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(NewArray);
+ChangeEven(NewArray);
+PrintArray(NewArray);
