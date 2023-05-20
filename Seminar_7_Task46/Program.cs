64057a79e0ebfe90ca58ab8e09c67a06e1075741
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-05-20 15:47:19 +0300

    Corrections

---
+++ Seminar_7_Task46/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 46. Задайте двумерный массив размером m x n, заполненный случайными целыми числами.
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+PrintArray(CreateTwoDimensionArray(6, 5));
