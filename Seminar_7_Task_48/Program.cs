78304f3bf3eb6e76d21e981caad6b05ec4132964
Viazmenskiy Stanislav <st-51b@mail.ru>
2023-05-20 15:45:10 +0300

    Seminar_7_Task_48

---
+++ Seminar_7_Task_48/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 48. Задайте двумерный массив размером m x n, Каждый элемент находится по формуле m+n.
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = i+j;
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+PrintArray(CreateTwoDimensionArray(3, 4));
